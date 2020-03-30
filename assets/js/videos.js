f113dbf3c9dcb314b079766592e86f49595e148c
Will <will@willm.xyz>
2020-03-30 18:07:09 -0400

    Update videos.js

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $.getJSON("data/youtube_videos.json", function(data){
     $.each(data.videos, function(index,video){
-      $("#video-container").append('<div class="column is-one-third-desktop is-half-tablet "><iframe id="ytplayer" type="text/html" style="float:none; margin-bottom: 10px;" width="854" height="480" src="https://www.youtube.com/embed/'+video.video_id+'?autoplay=0 frameborder="0"></iframe></div>');
+      $("#video-container").append('<div class="column is-one-third-desktop is-half-tablet "><iframe id="ytplayer" type="text/html" style="float:none; margin-bottom: 10px;" width="160" height="90" src="https://www.youtube.com/embed/'+video.video_id+'?autoplay=0 frameborder="0"></iframe></div>');
 
   });
 });
